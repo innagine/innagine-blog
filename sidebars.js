4f341dfc76744b9d258b3c5c7cb6f271774e3eab
innagine <NingDongOne@163.com>
2021-01-14 13:00:57 +0800

    原生ajax的更新

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,7 @@ module.exports = {
           "JS/js-dom",
           "JS/js-rubbish",
           "JS/js-loop",
+          "JS/js-ajax",
           "JS/js-set1",
           "JS/js-set2",
           "JS/js-set3",
