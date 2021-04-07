1816c3f80ffeb5d44539e46f653400753c0b843d
邓宁栋 <NingDongOne@163.com>
2021-04-07 11:35:47 +0800

    feat：更新js模块5篇文章

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@
  * @Author: IMAGINE
  * @Date: 2021-01-05 10:41:39
  * @LastEditors: IMAGINE
- * @LastEditTime: 2021-03-24 00:31:54
+ * @LastEditTime: 2021-04-07 11:34:03
  */
 /**
  * Copyright (c) 2017-present, Facebook, Inc.
@@ -47,6 +47,11 @@ module.exports = {
           "JS/js-gitflow",
           "JS/js-promise",
           "JS/js-promise2",
+          "JS/js-flat",
+          "JS/js-jsonp",
+          "JS/js-compose",
+          "JS/js-copy",
+          "JS/js-emitter",
           ],
   },
   videos: {
